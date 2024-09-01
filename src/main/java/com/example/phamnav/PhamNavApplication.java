edad616b00045113f6251c3441bbc4901e20bb6a
kec <weoncheol1@naver.com>
2024-09-01 21:52:56 +0900

    first commit

---
+++ src/main/java/com/example/phamnav/PhamNavApplication.java
@@ -0,0 +1,13 @@
+package com.example.phamnav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamNavApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhamNavApplication.class, args);
+    }
+
+}
