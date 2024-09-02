c9a69c8588831126192aa98ba61a1977c40d1f29
kec <weoncheol1@naver.com>
2024-09-02 22:41:44 +0900

    fix: KakaoApiResponseDto JSON 매핑 수정
    JSON 매핑을 위한 @JsonProperty 어노테이션 추가

--- src/main/java/com/example/phamnav/api/dto/KakaoApiResponseDto.java
+++ src/main/java/com/example/phamnav/api/dto/KakaoApiResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.phamnav.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class KakaoApiResponseDto {
-
+    @JsonProperty("meta")
     private MetaDto metaDto;
 
+    @JsonProperty("documents")
     private List<DocumentDto> documentList;
 }
