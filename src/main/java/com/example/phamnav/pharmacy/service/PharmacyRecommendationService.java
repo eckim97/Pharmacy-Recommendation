ec4dfeb122ce5be2c2ddd5aaaba41741257ab753
kec <weoncheol1@naver.com>
2024-09-04 06:25:01 +0900

    refactor: 약국 추천 기능을 위한 서비스 코드 수정
    PharmacyRecommendationService에서 주소를 기반으로 약국을 추천하고, 결과 리스트를 반환하는 메서드를 추가.
    카카오 API를 사용해 주소 검색 및 약국 추천 로직을 개선.
    추천된 약국 리스트를 OutputDto 형태로 변환하여 컨트롤러에 전달할 수 있도록 수정.

--- src/main/java/com/example/phamnav/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/phamnav/pharmacy/service/PharmacyRecommendationService.java
@@ -5,13 +5,16 @@ import com.example.phamnav.api.dto.KakaoApiResponseDto;
 import com.example.phamnav.api.service.KakaoAddressSearchService;
 import com.example.phamnav.direction.entity.Direction;
 import com.example.phamnav.direction.service.DirectionService;
+import com.example.phamnav.dto.OutputDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,20 +24,35 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacy(String address) {
+    public List<OutputDto> recommendPharmacyList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
             if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
                 log.error("[PharmacyRecommendationService] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
+        // 공공기관 약국 데이터
+        List<Direction> directionList = directionService.buildDriectionList(documentDto);
 
-//        List<Direction> directionList = directionService.buildDriectionList(documentDto);
-        List<Direction> directionsList1 = directionService.buildDirectionListByCategoryApi(documentDto);
-        directionService.saveAll(directionsList1);
+        // kakao api
+//        List<Direction> directionsList1 = directionService.buildDirectionListByCategoryApi(documentDto);
 
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(this::convertToOutputDto)
+                .collect(Collectors.toList());
     }
+    private OutputDto convertToOutputDto(Direction direction) {
+        return OutputDto.builder()
+                .pharmacyAddress(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl("todo") //TODO
+                .roadViewUrl("todo")
+                .distance(String.format("%.2f", direction.getDistance()))
+                .build();
+    }
+
 }
