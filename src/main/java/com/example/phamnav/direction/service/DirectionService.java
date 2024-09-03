7664db3552b15f0e7ff69e07cd6d74939b3eb9c5
kec <weoncheol1@naver.com>
2024-09-03 22:39:50 +0900

    refactor: 약국 검색을 위해 DirectionService에서 카카오 API 사용하도록 수정
    기존의 공공데이터를 사용한 약국 검색 로직을 카카오 API 기반으로 대체.

--- src/main/java/com/example/phamnav/direction/service/DirectionService.java
+++ src/main/java/com/example/phamnav/direction/service/DirectionService.java
@@ -1,6 +1,7 @@
 package com.example.phamnav.direction.service;
 
 import com.example.phamnav.api.dto.DocumentDto;
+import com.example.phamnav.api.service.KakaoCategorySearchService;
 import com.example.phamnav.direction.entity.Direction;
 import com.example.phamnav.direction.repository.DirectionRepository;
 import com.example.phamnav.pharmacy.service.PharmacySearchService;
@@ -25,6 +26,7 @@ public class DirectionService {
 
     private final PharmacySearchService pharmacySearchService;
     private final DirectionRepository directionRepository;
+    private final KakaoCategorySearchService kakaoCategorySearchService;
 
     @Transactional
     public List<Direction> saveAll(List<Direction> directionList) {
@@ -55,6 +57,28 @@ public class DirectionService {
                 .limit(MAX_SEARCH_COUNT)
                 .collect(Collectors.toList());
 
+    }
+
+    // pharmacy search by category kakao api
+    public List<Direction> buildDirectionListByCategoryApi(DocumentDto inputDocumentDto) {
+        if(Objects.isNull(inputDocumentDto)) return Collections.emptyList();
+
+        return kakaoCategorySearchService
+                .requestPharmacyCategorySearch(inputDocumentDto.getLatitude(), inputDocumentDto.getLongitude(), RADIUS_KM)
+                .getDocumentList()
+                .stream().map(resultDocumentDto ->
+                        Direction.builder()
+                                .inputAddress(inputDocumentDto.getAddressName())
+                                .inputLatitude(inputDocumentDto.getLatitude())
+                                .inputLongitude(inputDocumentDto.getLongitude())
+                                .targetPharmacyName(resultDocumentDto.getPlaceName())
+                                .targetAddress(resultDocumentDto.getAddressName())
+                                .targetLatitude(resultDocumentDto.getLatitude())
+                                .targetLongitude(resultDocumentDto.getLongitude())
+                                .distance(resultDocumentDto.getDistance() * 0.001) // km 단위
+                                .build())
+                .limit(MAX_SEARCH_COUNT)
+                .collect(Collectors.toList());
     }
 
     // Haversine formula
